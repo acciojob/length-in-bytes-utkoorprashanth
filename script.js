5ae579c1e4c553a0bb6b0645e5d2df7e7c920242
utkoorprashanth <137163756+utkoorprashanth@users.noreply.github.com>
2026-02-05 22:36:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
 const byteSize = (str) => {
-  // write your code here
+  // write your code 
 	function byteSize(str) {
-  // Create a Blob object with the input string
-  const blob = new Blob([str]);
-  
-  // Return the size property, which gives the byte length
+		const Blob = new Blob([str])
+	}
   return blob.size;
 		
 }
-
 };
-console.log(byteSize('hello world')); // Output: 11
-console.log(byteSize('안녕하세요'));   // Output: 15
-console.log(byteSize(''));            // Output: 0
 
 // Do not change the code below
 const str = prompt("Enter some string.");
